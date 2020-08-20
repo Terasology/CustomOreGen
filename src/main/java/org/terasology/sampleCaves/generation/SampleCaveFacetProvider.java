1bd6982ea9d3e5b87109702806ff49f90aa7c9ba
DarkWeird <foxflameless@gmail.com>
2020-08-20 21:35:20 +0300

    fix: NUI extraction compatibility

--- src/main/java/org/terasology/sampleCaves/generation/SampleCaveFacetProvider.java
+++ src/main/java/org/terasology/sampleCaves/generation/SampleCaveFacetProvider.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.sampleCaves.generation;
 
 import org.terasology.customOreGen.PDist;
@@ -20,7 +7,7 @@ import org.terasology.customOreGen.Structure;
 import org.terasology.customOreGen.StructureDefinition;
 import org.terasology.customOreGen.VeinsStructureDefinition;
 import org.terasology.entitySystem.Component;
-import org.terasology.rendering.nui.properties.Range;
+import org.terasology.nui.properties.Range;
 import org.terasology.world.generation.ConfigurableFacetProvider;
 import org.terasology.world.generation.FacetProvider;
 import org.terasology.world.generation.GeneratingRegion;
